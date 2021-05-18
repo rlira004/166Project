eafa5845bfe54aa3336832303ea014bda91f5586
Ricardo <63069526+rlira004@users.noreply.github.com>
2021-05-17 20:04:46 -0700

    Update ProjPhase2.sql

--- ProjPhase2.sql
+++ ProjPhase2.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS Hospital;
-DROP TABLE IF EXISTS Department;
-DROP TABLE IF EXISTS Doctor;
-DROP TABLE IF EXISTS Staff;
-DROP TABLE IF EXISTS Request_Maintenance;
-DROP TABLE IF EXISTS Appointment;
-DROP TABLE IF EXISTS Past;
-DROP TABLE IF EXISTS Active;
-DROP TABLE IF EXISTS Available;
-DROP TABLE IF EXISTS Waitlisted;
-DROP TABLE IF EXISTS Patient;
+DROP TABLE Hospital CASCADE;
+DROP TABLE Department CASCADE;
+DROP TABLE Doctor CASCADE;
+DROP TABLE Staff CASCADE;
+DROP TABLE Request_Maintenance CASCADE;
+DROP TABLE Appointment CASCADE;
+DROP TABLE Past CASCADE;
+DROP TABLE Active CASCADE;
+DROP TABLE Available CASCADE;
+DROP TABLE Waitlisted CASCADE;
+DROP TABLE Patient CASCADE;
 
 CREATE TABLE Hospital(
 hospital_ID CHAR(20),
@@ -18,7 +18,7 @@ PRIMARY KEY (hospital_ID)
 
 CREATE TABLE Department(
 dept_ID CHAR(20),
-hospital_ID CHAR(20),		//Department is many, therefore hospital_ID
+hospital_ID CHAR(20),
 name CHAR(20),
 PRIMARY KEY (dept_ID),
 FOREIGN KEY (hospital_ID) REFERENCES Hospital (hospital_ID)
@@ -26,7 +26,7 @@ FOREIGN KEY (hospital_ID) REFERENCES Hospital (hospital_ID)
 
 CREATE TABLE Doctor(
 doctor_ID CHAR(20),
-dept_ID CHAR(20),		//Doctor is many, therefore Department_ID
+dept_ID CHAR(20),
 name CHAR(20),
 specialty CHAR(20),
 PRIMARY KEY (doctor_ID),
@@ -35,11 +35,15 @@ FOREIGN KEY (dept_ID) REFERENCES Hospital (hospital_ID)
 
 CREATE TABLE Staff(
 staff_ID CHAR(20),
-hospital_ID CHAR(20),		//is many, therefore hospital_ID
+hospital_ID CHAR(20),
 name CHAR(20),
 PRIMARY KEY (staff_ID),
 FOREIGN KEY (hospital_ID) REFERENCES Hospital (hospital_ID)
 );
+
+
+
+
 
 CREATE TABLE Appointment(
 appnt_ID CHAR(20),
@@ -48,14 +52,6 @@ time_slot CHAR(20),
 type ENUM("past", "active", "available", "waitlisted") NOT NULL,
 PRIMARY KEY (appnt_ID),
 );
-
-
-
-//This website says to do IS_A this way, idk
-//https://dba.stackexchange.com/questions/5501/how-do-i-map-an-is-a-relationship-into-a-database
-//
-
-
 
 CREATE TABLE Past(
 appnt_ID CHAR(20),
@@ -93,15 +89,6 @@ PRIMARY KEY (appnt_ID),
 FOREIGN KEY (appnt_ID, date, time_slot, type) REFERENCES Appointment (appnt_ID, date, time_slot, type)
 );
 
-
-
-//This website says to do IS_A this way, idk
-//https://dba.stackexchange.com/questions/5501/how-do-i-map-an-is-a-relationship-into-a-database
-//
-
-
-
-
 CREATE TABLE Patient(
 patient_ID CHAR(20),
 name CHAR(20),
@@ -109,10 +96,10 @@ gender CHAR(20),
 age CHAR(3),
 address CHAR(20)
 #appointments INTEGER,
-PRIMARY KEY (appnt_ID)
+PRIMARY KEY (patient_ID)
 );
 
-CREATE TABLE Request_Maintenance(		//gets table cuz many2many
+CREATE TABLE Request_Maintenance(
 doctor_ID CHAR(20),
 staff_ID CHAR(20),
 patient_per_hour CHAR(20),
